2f1331cd8767460027df8caf6e77073acf2a383b
Coredex <73810221+coredex-source@users.noreply.github.com>
2024-10-23 22:08:40 +0530

    v23.10.24
    - Changed how commits are named
    - Fixed testcase generation
    - Implemented initial output generation algorithm
    - test.py now contains a test for the new algorithm

--- cogs/GenerationAlgorithms.py
+++ cogs/GenerationAlgorithms.py
@@ -1,4 +1,5 @@
 import random
+import subprocess
 
 def generate_testcases(testcase1, testcase2, output_cases=5):
     """
@@ -26,7 +27,10 @@ def generate_testcases(testcase1, testcase2, output_cases=5):
         elif isinstance(testcase, tuple):
             return tuple(generate_similar_structure(element) for element in testcase)
         elif isinstance(testcase, (int, float)):
-            return random.randint(0, 100) if isinstance(testcase, int) else random.uniform(0, 100)
+            if testcase < 10:
+                return random.randint(0, 10) if isinstance(testcase, int) else random.uniform(0, 10)
+            else:
+                return random.randint(0, 100) if isinstance(testcase, int) else random.uniform(0, 100)
         elif isinstance(testcase, str):
             length = len(testcase)
             return ''.join(random.choices('abcdefghijklmnopqrstuvwxyz', k=length))
@@ -39,14 +43,33 @@ def generate_testcases(testcase1, testcase2, output_cases=5):
 
     return test_cases
 
-def generate_expectedOutputs(array_tc):
-    pass
+def generate_expectedOutputs(array_tc,data,return_data):
+    result_data = []
+    for testcase in array_tc:
+        data_dict = {}
+        for i in range(len(return_data)):
+            data_dict[return_data[i]] = testcase[i]
+
+        exec_stat = ["python", "-c", data]
+        for i in data_dict:
+            exec_stat.append(str(data_dict[i]))
+            
+        # Running the Python code using subprocess and passing arguments
+        result = subprocess.run(
+            exec_stat,
+            text=True,
+            capture_output=True
+        )
+
+        output = result.stdout or result.stderr
+
+        result_data.append(output)
+    return result_data
 
 def generate_tc_a_pairs(testcase1, testcase2, output_cases=5):
     base_array_tc = generate_testcases(testcase1,testcase2, output_cases)
     array_tc = sort_array(base_array_tc)
-    print(array_tc)
-    print(len(array_tc))
+    return array_tc
 
 def sort_array(array_tc):
     sorted_array_tc = []
@@ -55,5 +78,3 @@ def sort_array(array_tc):
             sorted_array_tc.append(i)
 
     return sorted_array_tc
-
-#generate_tc_a_pairs(117, 21, 65)
--- cogs/sql.py
+++ cogs/sql.py
@@ -55,10 +55,48 @@ def fetch_output(host,user,password,database,table,serial_number):
             database=database
         )
         cursor = conn.cursor()
-        cursor.execute(f'SELECT Answer FROM {table} where SNo = \"{serial_number}\"')
+        cursor.execute(f'SELECT Answer FROM {table} where SNo = {serial_number}')
         data = cursor.fetchall()
         expectedOutput = data[0][0]
         conn.close()
         return str(expectedOutput)
+    except mysql.connector.Error as err:
+        return f"Error: {err}"
+
+def fetch_code(host,user,password,database,table,serial_number):
+    '''
+    Parameters:
+    - host: SQL hostname
+    - user: SQL username
+    - password: SQL password
+    - database: SQL database name
+    - table: SQL table name
+    Returns:
+    - Fetched data from SQL [Question, hints and expectedOutputs]
+    '''
+    try:
+        conn = mysql.connector.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=database
+        )
+        cursor = conn.cursor()
+        cursor.execute(f'SELECT code from {table} where SNo = {serial_number}')
+        data = cursor.fetchone()
+        data = data[0]
+        new_data = data.split("\n")
+        for i in range(len(new_data)):
+            if "def main" in new_data[i]:
+                new_data = new_data[i]
+                new_data = new_data.rsplit("(")
+                new_data = new_data[1]
+                new_data = new_data.rsplit(")")
+                new_data = new_data[0]
+                return_data = []
+                for i in new_data:
+                    if i != "," and i != " ":
+                        return_data.append(i)
+                return data, return_data
     except mysql.connector.Error as err:
         return f"Error: {err}"
